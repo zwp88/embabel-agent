ca6128c0b70842966428e095373a2add66aadef7
Rod Johnson <johnsonroda@gmail.com>
2025-04-10 15:13:26 +1000

    First integration test

--- src/test/kotlin/com/embabel/agent/e2e/AgentPlatformIntegrationTest.kt
+++ src/test/kotlin/com/embabel/agent/e2e/AgentPlatformIntegrationTest.kt
@@ -22,6 +22,9 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
+/**
+ * Integration tests
+ */
 @SpringBootTest
 class AgentPlatformIntegrationTest(
     @Autowired
