a98f72124a78ca6869cb4f4f634f808c519471b5
Rod Johnson <johnsonroda@gmail.com>
2025-04-20 13:02:55 +1000

    Move AgentPlatform running utils

--- src/main/kotlin/com/embabel/agent/core/AgentPlatform.kt
+++ src/main/kotlin/com/embabel/agent/core/AgentPlatform.kt
@@ -15,17 +15,10 @@
  */
 package com.embabel.agent.core
 
-import com.embabel.agent.domain.special.UserInput
 import com.embabel.agent.event.AgenticEventListener
-import com.embabel.agent.event.DynamicAgentCreationEvent
-import com.embabel.agent.event.RankingChoiceRequestEvent
 import com.embabel.agent.spi.Ranker
-import com.embabel.agent.spi.Rankings
 import com.embabel.agent.spi.ToolGroupResolver
-import com.embabel.agent.testing.FakeRanker
-import com.embabel.agent.testing.RandomRanker
 import com.embabel.common.core.types.ZeroToOne
-import com.embabel.common.util.kotlin.loggerFor
 
 /**
  * Controls log output.
@@ -57,30 +50,6 @@ data class ProcessOptions(
     val allowGoalChange: Boolean = true,
     val maxActions: Int = 40,
 )
-
-/**
- * Result of directly trying to execute a goal
- * Forcing client to discriminate
- */
-sealed interface DynamicExecutionResult {
-    val basis: Any
-
-    data class Success(
-        override val basis: Any,
-        val output: Any,
-        val agentProcessStatus: AgentProcessStatus,
-    ) : DynamicExecutionResult
-
-    data class NoGoalFound(
-        override val basis: Any,
-        val goalRankings: Rankings<Goal>,
-    ) : DynamicExecutionResult
-
-    data class NoAgentFound(
-        override val basis: Any,
-        val agentRankings: Rankings<Agent>,
-    ) : DynamicExecutionResult
-}
 
 /**
  * Properties common to all AgentPlatform implementations
@@ -181,166 +150,4 @@ interface AgentPlatform : AgentScope {
     override val conditions: Set<Condition>
         get() = agents().flatMap { it.conditions }.distinctBy { it.name }.toSet()
 
-
-    /**
-     * Choose a goal based on the user input and try to achieve it.
-     * Open execution model:
-     * May bring in actions and conditions from multiple agents to help achieve the goal.
-     * Doesn't need reified types because we don't know the type yet.
-     */
-    fun chooseAndAccomplishGoal(
-        intent: String,
-        processOptions: ProcessOptions = ProcessOptions(),
-    ): DynamicExecutionResult {
-        val userInput = UserInput(intent)
-
-        // Use a fake goal ranker if we are in test mode and don't already have a fake one
-        // Enables running under integration tests and in test mode otherwise with production config
-        val rankerToUse = if (processOptions.test && ranker !is FakeRanker) {
-            RandomRanker()
-        } else {
-            ranker
-        }
-
-        val goalChoiceEvent = RankingChoiceRequestEvent<Goal>(
-            agentPlatform = this,
-            type = Goal::class.java,
-            basis = userInput,
-            choices = this.goals,
-        )
-        eventListener.onPlatformEvent(goalChoiceEvent)
-        val goalRankings = rankerToUse
-            .rankGoals(userInput = userInput, goals = this.goals)
-        val credibleGoals = goalRankings
-            .rankings
-            .filter { it.confidence > properties.goalConfidenceCutOff }
-        val goalChoice = credibleGoals.firstOrNull()
-        if (goalChoice == null) {
-            eventListener.onPlatformEvent(
-                goalChoiceEvent.noDeterminationEvent(
-                    rankings = goalRankings,
-                    confidenceCutoff = properties.goalConfidenceCutOff
-                )
-            )
-            return DynamicExecutionResult.NoGoalFound(goalRankings = goalRankings, basis = userInput)
-        }
-
-        loggerFor<AgentPlatform>().debug(
-            "Goal choice {} with confidence {} for user intent {}: Choices were {}",
-            goalChoice.ranked.name,
-            goalChoice.confidence,
-            intent,
-            goals.joinToString("\n") { it.name },
-        )
-        eventListener.onPlatformEvent(
-            goalChoiceEvent.determinationEvent(
-                choice = goalChoice,
-                rankings = goalRankings,
-            )
-        )
-
-        val goalAgent = createAgent(
-            name = "goal-${goalChoice.ranked.name}",
-            description = goalChoice.ranked.description,
-        )
-            .withSingleGoal(goalChoice.ranked)
-        eventListener.onPlatformEvent(
-            DynamicAgentCreationEvent(
-                agent = goalAgent,
-                agentPlatform = this,
-                basis = userInput,
-            )
-        )
-        val processStatus = runAgentFrom(
-            processOptions = processOptions,
-            agent = goalAgent,
-            bindings = mapOf(
-                "it" to userInput
-            )
-        )
-        return DynamicExecutionResult.Success(
-            basis = userInput,
-            output = processStatus.finalResult()!!,
-            agentProcessStatus = processStatus,
-        )
-    }
-
-    /**
-     * Choose an agent based on the user input and run it.
-     * Closed execution model:
-     * Will never mix actions and goals from different agents.
-     * Doesn't need reified types because we don't know the type yet.
-     */
-    fun chooseAndRunAgent(
-        intent: String,
-        processOptions: ProcessOptions = ProcessOptions(),
-    ): DynamicExecutionResult {
-        val userInput = UserInput(intent)
-
-        // Use a fake ranker if we are in test mode and don't already have a fake one
-        // Enables running under integration tests and in test mode otherwise with production config
-        val rankerToUse = if (processOptions.test && ranker !is FakeRanker) {
-            RandomRanker()
-        } else {
-            ranker
-        }
-
-        val agentChoiceEvent = RankingChoiceRequestEvent<Agent>(
-            agentPlatform = this,
-            type = Agent::class.java,
-            basis = userInput,
-            choices = this.agents(),
-        )
-        eventListener.onPlatformEvent(agentChoiceEvent)
-        val agentRankings = rankerToUse
-            .rankAgents(userInput = userInput, agents = this.agents())
-        val credibleAgents = agentRankings
-            .rankings
-            .filter { it.confidence > properties.agentConfidenceCutOff }
-        val agentChoice = credibleAgents.firstOrNull()
-        if (agentChoice == null) {
-            eventListener.onPlatformEvent(
-                agentChoiceEvent.noDeterminationEvent(
-                    rankings = agentRankings,
-                    confidenceCutoff = properties.agentConfidenceCutOff
-                )
-            )
-            return DynamicExecutionResult.NoAgentFound(agentRankings = agentRankings, basis = userInput)
-        }
-
-        loggerFor<AgentPlatform>().debug(
-            "Agent choice {} with confidence {} for user intent {}: Choices were {}",
-            agentChoice.ranked.name,
-            agentChoice.confidence,
-            intent,
-            agents().joinToString("\n") { it.name },
-        )
-        eventListener.onPlatformEvent(
-            agentChoiceEvent.determinationEvent(
-                choice = agentChoice,
-                rankings = agentRankings,
-            )
-        )
-
-        val agent = agentChoice.ranked
-        eventListener.onPlatformEvent(
-            DynamicAgentCreationEvent(
-                agent = agentChoice.ranked,
-                agentPlatform = this,
-                basis = userInput,
-            )
-        )
-        val processStatus = runAgentFrom(
-            processOptions = processOptions,
-            agent = agent,
-            bindings = mapOf(
-                "it" to userInput
-            )
-        )
-        return DynamicExecutionResult.Success(
-            basis = userInput,
-            output = processStatus.finalResult()!!,
-            agentProcessStatus = processStatus,
-        )
-    }
 }
