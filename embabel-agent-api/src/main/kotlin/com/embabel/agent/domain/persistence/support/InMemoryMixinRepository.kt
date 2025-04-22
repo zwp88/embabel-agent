a8550b012153bb395bc657289fa728bbb15123f6
Rod Johnson <johnsonroda@gmail.com>
2025-04-22 15:47:11 +1000

    Pull out MixinManager

--- embabel-agent-api/src/main/kotlin/com/embabel/agent/domain/persistence/support/InMemoryMixinRepository.kt
+++ embabel-agent-api/src/main/kotlin/com/embabel/agent/domain/persistence/support/InMemoryMixinRepository.kt
@@ -1,7 +1,26 @@
+/*
+ * Copyright 2024-2025 Embabel Software, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.embabel.agent.domain.persistence.support
 
+import com.embabel.agent.domain.mixin.MixinHolder
+import com.embabel.agent.domain.mixin.MixinManager
 import com.embabel.agent.domain.persistence.MixinEnabledEntity
 import java.util.*
+
+typealias MixinEnabledEntityHolder = MixinManager<MixinEnabledEntity<String>>
 
 class InMemoryMixinRepository : AbstractMixinRepository<String>(
     generateId = { UUID.randomUUID().toString() },
@@ -11,10 +30,9 @@ class InMemoryMixinRepository : AbstractMixinRepository<String>(
 
     override fun store(entity: MixinEnabledEntity<String>, id: String) {
         var found = map[id]
-        val entityHolder = found?.copy(root = entity)
+        val entityHolder = found?.updateRoot(root = entity)
             ?: MixinEnabledEntityHolder(
                 root = entity,
-                mixins = emptyList(),
             )
         map[id] = entityHolder
     }
@@ -43,8 +61,8 @@ class InMemoryMixinRepository : AbstractMixinRepository<String>(
             mixin = t,
         )
         // TODO change the mixin if it already exists
-        val mixins = foundEntityHolder.mixins + mixin
-        map[e.id!!] = foundEntityHolder.copy(mixins = mixins)
+        val mixins = foundEntityHolder + mixin
+        map[e.id!!] = mixins
         return e
     }
 
---
+++ embabel-agent-api/src/test/kotlin/com/embabel/agent/domain/mixin/MixinManagerTest.kt
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2024-2025 Embabel Software, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.embabel.agent.domain.mixin
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+interface Animal {
+    val name: String
+}
+
+interface Dog : Animal {
+    val breed: String
+
+    companion object {
+
+        operator fun invoke(name: String, breed: String): Dog = DogImpl(name, breed)
+    }
+}
+
+private data class DogImpl(override val name: String, override val breed: String) : Dog
+
+interface Cat : Animal {
+    val color: String
+
+    companion object {
+
+        operator fun invoke(name: String, color: String): Cat = CatImpl(name, color)
+    }
+}
+
+private data class CatImpl(override val name: String, override val color: String) : Cat
+
+interface Insured {
+    val policyNumber: String
+
+    companion object {
+
+        operator fun invoke(policyNumber: String): Insured = InsuredImpl(policyNumber)
+    }
+}
+
+private data class InsuredImpl(override val policyNumber: String) : Insured
+
+interface InsuredDog : Dog, Insured {
+}
+
+
+class MixinManagerTest {
+
+    @Test
+    fun `leaves root type alone`() {
+        val dog = Dog("Rex", "Labrador")
+        val mixinManager = MixinManager(dog)
+        mixinManager.implementationOfAllInterfaces() is Dog
+        assertEquals(dog.name, mixinManager.implementationOfAllInterfaces().name)
+    }
+
+    @Test
+    fun `adds insured with reification and interface detection`() {
+        val dog = Dog("Rex", "Labrador")
+        val insured = Insured("123456")
+        val mixinManager = MixinManager(dog)
+        mixinManager.implementationOfAllInterfaces() is Dog
+        assertFalse(mixinManager.implementationOfAllInterfaces() is Insured)
+        mixinManager.becomeFirstInterface(insured)
+        val impl = mixinManager.implementationOfAllInterfaces()
+        impl as Insured
+        assertEquals(dog.name, impl.name)
+        assertEquals(insured.policyNumber, impl.policyNumber)
+        assertEquals(setOf(Dog::class.java, Insured::class.java), mixinManager.allInterfaces().toSet())
+    }
+
+    @Test
+    fun `adds insured with explicit interface`() {
+        val dog = Dog("Rex", "Labrador")
+        val insured = Insured("123456")
+        val mixinManager = MixinManager(dog)
+        mixinManager.implementationOfAllInterfaces() is Dog
+        assertFalse(mixinManager.implementationOfAllInterfaces() is Insured)
+        mixinManager.become(insured, Insured::class.java)
+        val impl = mixinManager.implementationOfAllInterfaces()
+        impl as Insured
+        assertEquals(dog.name, impl.name)
+        assertEquals(insured.policyNumber, impl.policyNumber)
+        assertEquals(setOf(Dog::class.java, Insured::class.java), mixinManager.allInterfaces().toSet())
+    }
+
+}
