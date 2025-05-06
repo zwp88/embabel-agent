92fe5c23d93c96ed83c545500f7ef59669824593
Rod Johnson <johnsonroda@gmail.com>
2025-05-05 22:05:52 -0700

    Pythons

---
+++ embabel-agent-api/src/main/kotlin/com/embabel/agent/event/logging/personality/montypython/StarwarsPromptProvider.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2024-2025 Embabel Software, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.embabel.agent.event.logging.personality.montypython
+
+import com.embabel.agent.event.logging.personality.ColorPalette
+import com.embabel.agent.shell.MessageGeneratorPromptProvider
+import com.embabel.common.util.RandomFromFileMessageGenerator
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
+
+@Component
+@Profile("montypython")
+object MontyPythonColors : ColorPalette {
+    const val MEMBRANE: Int = 0xbeb780
+    const val GREEN: Int = 0x7da17e
+
+    override val highlight: Int
+        get() = MEMBRANE
+    override val color2: Int
+        get() = GREEN
+}
+
+
+@Component
+@Profile("montypython")
+class MontyPythonPromptProvider : MessageGeneratorPromptProvider(
+    color = MontyPythonColors.MEMBRANE,
+    prompt = "pythons",
+    messageGenerator = RandomFromFileMessageGenerator(
+        url = "logging/montypython.txt"
+    ),
+)
