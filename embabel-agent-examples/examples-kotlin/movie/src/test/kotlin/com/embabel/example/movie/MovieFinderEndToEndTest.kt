af33062cc8184b6dceb6037a3065f6d456ecfbed
Rod Johnson <johnsonroda@gmail.com>
2025-06-03 13:56:33 +0200

    Delete duplicated MovieFinder and move test

--- embabel-agent-examples/examples-kotlin/movie/src/test/kotlin/com/embabel/example/movie/MovieFinderEndToEndTest.kt
+++ embabel-agent-examples/examples-kotlin/movie/src/test/kotlin/com/embabel/example/movie/MovieFinderEndToEndTest.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.embabel.agent.e2e
+package com.embabel.example.movie
 
 import com.embabel.agent.api.annotation.support.AgentMetadataReader
 import com.embabel.agent.core.Agent
