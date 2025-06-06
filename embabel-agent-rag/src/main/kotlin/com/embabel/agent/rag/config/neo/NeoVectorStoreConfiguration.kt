e7c0b21c1deaa7dbfa8deb5694e8ee716df2a3ea
Rod Johnson <johnsonroda@gmail.com>
2025-06-06 20:47:15 +0400

    Correct wrong package name

--- embabel-agent-rag/src/main/kotlin/com/embabel/agent/rag/config/neo/NeoVectorStoreConfiguration.kt
+++ embabel-agent-rag/src/main/kotlin/com/embabel/agent/rag/config/neo/NeoVectorStoreConfiguration.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.embabel.agent.config.neo
+package com.embabel.agent.rag.config.neo
 
 import com.embabel.common.ai.model.EmbeddingService
 import org.neo4j.driver.Driver
