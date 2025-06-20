8d35123cae61e4ce207efe3025889064d28b4774
Igor Dayen <igor.dayen@gmail.com>
2025-06-19 20:45:20 -0400

    addition of missing licenses

--- embabel-agent-autoconfigure/src/main/java/com/embabel/agent/config/annotation/EnableAgents.java
+++ embabel-agent-autoconfigure/src/main/java/com/embabel/agent/config/annotation/EnableAgents.java
@@ -1,4 +1,47 @@
+/*
+ * Copyright 2024-2025 Embabel Software, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.embabel.agent.config.annotation;
 
+import com.embabel.agent.autoconfigure.platform.AgentPlatformAutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * Annotation for  Enabling Agent AutoConfiguration and setting AgentApplication Active spring profile.
+ */
+@Retention(RetentionPolicy.RUNTIME) // Keep the annotation at runtime for reflection
+@Target(ElementType.TYPE)           // Apply the annotation to classes/types
+@ConfigurationPropertiesScan(
+        basePackages = {
+                "com.embabel.agent"
+        }
+)
+@ComponentScan(
+        basePackages = {
+                "com.embabel.agent.autoconfigure"
+        }
+)
+@ImportAutoConfiguration(classes={AgentPlatformAutoConfiguration.class})
 public @interface EnableAgents {
+    String[] value() default {"default"};
 }
--- embabel-agent-autoconfigure/src/main/java/com/embabel/agent/config/annotation/spi/EmbabelEnvironmentProcessor.java
+++ embabel-agent-autoconfigure/src/main/java/com/embabel/agent/config/annotation/spi/EmbabelEnvironmentProcessor.java
@@ -1,4 +1,87 @@
+/*
+ * Copyright 2024-2025 Embabel Software, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.embabel.agent.config.annotation.spi;
 
-public class EmbabelEnvironmentProcessor {
+import com.embabel.agent.config.annotation.EnableAgents;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.core.annotation.AnnotationAttributes;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+
+/**
+ * Sets Spring Active Profile from [EnableAgents] annotation.
+ */
+public class EmbabelEnvironmentProcessor implements EnvironmentPostProcessor, Ordered {
+
+    private Logger logger = LoggerFactory.getLogger(EmbabelEnvironmentProcessor.class);
+
+    private final static String SPRING_PROFILES_ACTIVE = "spring.profiles.active";
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        String[] profiles = findProfilesFromAnnotation(application);
+        if (profiles.length > 0) {
+            // Set system property (mimics export SPRING_PROFILES_ACTIVE)
+            String existingProfiles = System.getProperty(SPRING_PROFILES_ACTIVE);
+            String newProfiles = String.join(",", profiles);
+
+            if (existingProfiles != null && !existingProfiles.isEmpty()) {
+                System.setProperty(SPRING_PROFILES_ACTIVE, existingProfiles + "," + newProfiles);
+            } else {
+                System.setProperty("spring.profiles.active", newProfiles);
+            }
+            logger.info("Set Active Profile as {}", System.getProperty(SPRING_PROFILES_ACTIVE));
+        }
+    }
+
+    private String[] findProfilesFromAnnotation(SpringApplication application) {
+        for (Object source : application.getAllSources()) {
+            if (source instanceof Class) {
+
+                /*
+                 * Generic EmbabelAgents annotation
+                 */
+                EnableAgents enableProfile = AnnotationUtils.findAnnotation((Class<?>) source, EnableAgents.class);
+                /*
+                 * Get "value" attribute of child annotation  by hierarchy, such as EnableAgentShell, etc.
+                 */
+                AnnotationAttributes mergedAnnotationAttributes = AnnotatedElementUtils.getMergedAnnotationAttributes(
+                        (Class) source, EnableAgents.class);
+
+                if (mergedAnnotationAttributes != null && !mergedAnnotationAttributes.isEmpty()) {
+                    return mergedAnnotationAttributes.getStringArray("value");
+                } else if (enableProfile != null) {
+                    return enableProfile.value();
+                }
+
+            }
+        }
+        return new String[0];
+    }
+
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
 }
+
