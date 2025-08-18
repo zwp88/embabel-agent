8b3b5a527072160d4f54921a2f4c03f4571382f1
Rod Johnson <johnsonroda@gmail.com>
2025-08-18 16:31:56 +1000

    Polishing

--- embabel-agent-rag/src/test/kotlin/com/embabel/agent/rag/ogm/OgmCypherSearchTest.kt
+++ embabel-agent-rag/src/test/kotlin/com/embabel/agent/rag/ogm/OgmCypherSearchTest.kt
@@ -15,22 +15,22 @@
  */
 package com.embabel.agent.rag.ogm
 
-import com.embabel.test.NeoIntegrationTest
 import com.embabel.agent.rag.neo.ogm.OgmCypherSearch
+import com.embabel.test.NeoIntegrationTest
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 
 
-
 @NeoIntegrationTest
 class OgmCypherSearchTest(
-    @Autowired private val ogmCypherSearch: OgmCypherSearch) {
+    @param:Autowired private val ogmCypherSearch: OgmCypherSearch,
+) {
 
     @Test
     fun should_query() {
         val query = "match (n) return n limit 100"
-        val params : Map<String, *> = emptyMap<String, Any>()
+        val params: Map<String, *> = emptyMap<String, Any>()
         val result = ogmCypherSearch.query("purpose", query, params, null)
         println("Got result: $result")
         assertNotNull(result)
